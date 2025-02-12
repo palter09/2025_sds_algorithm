7fc3db8fd6723859eac1d989364e74b2f0f77fad
palter09 <42296211+palter09@users.noreply.github.com>
2025-02-12 17:29:51 +0900

    Create 플로이드워셜.java

---
+++ 8일차/정답/플로이드워셜.java
@@ -0,0 +1,61 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+/**
+5 8
+1 3 5
+1 2 7
+1 4 -3
+3 5 3
+3 4 3
+4 2 3
+4 5 6
+5 3 2
+ */
+public class Main {
+	
+	static int V, E, adj[][], INF=987654;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader  br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		
+		V = Integer.parseInt(st.nextToken());
+		E = Integer.parseInt(st.nextToken());
+		adj = new int[V+1][V+1];
+		for (int i=1; i<=V; i++) {
+			for (int j=1; j<=V; j++) {
+				adj[i][j] = INF;
+				if (i == j) {
+					adj[i][j] = 0;
+				}
+			}
+		}
+		int s, e, c;
+		for (int i=0; i<E; i++) {
+			st = new StringTokenizer(br.readLine());
+			s = Integer.parseInt(st.nextToken());
+			e = Integer.parseInt(st.nextToken());
+			c = Integer.parseInt(st.nextToken());
+			adj[s][e] = c;
+		}
+		// k 경유지 i 시작점 j 도착점
+		for (int k=1; k<=V; k++) {
+			for (int i=1; i<=V; i++) {
+				for (int j=1; j<=V; j++) {
+					if (adj[i][k] != INF && adj[k][j] != INF) {
+						adj[i][j] = Math.min(adj[i][j], adj[i][k] + adj[k][j]);
+					}
+				}
+			}
+		}
+		for (int i=1; i<=V; i++) {
+			for (int j=1; j<=V; j++) {
+				System.out.print(adj[i][j]+ " ");
+			}
+			System.out.println();
+		}	
+	}
+}
