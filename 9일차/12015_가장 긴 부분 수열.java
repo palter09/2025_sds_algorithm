b40f6752065f9fce6b86c905d542c2641dac0f9f
palter09 <42296211+palter09@users.noreply.github.com>
2025-02-13 10:23:14 +0900

    12015_가장 긴 부분 수열.java 업데이트

--- 9일차/12015_가장 긴 부분 수열.java
+++ 9일차/12015_가장 긴 부분 수열.java
@@ -1,62 +1,53 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.PriorityQueue;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
+    static int N;
+    static int[] A;
+    static int[] dp; // dp[i] : 길이가 i+1인 증가 부분 수열의 마지막 원소(최소값)
 
-	static int N;
-	static int [] A;
-	static int [] longS;
-	
     public static void main(String[] args) throws IOException  {
-           BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-           StringTokenizer st = new StringTokenizer(br.readLine());
-           StringBuffer sb = new StringBuffer();
-           
-           N = Integer.parseInt(st.nextToken());
-           
-           A = new int[N+1];
-           longS = new int[N+1];
-           
-           st = new StringTokenizer(br.readLine());
-           for (int i=0; i<N; i++) {
-        	   A[i] = Integer.parseInt(st.nextToken());
-        	   longS[i] = 1000000;
-           }
-           
-           for (int i=0; i<N; i++) {
-        	   int idx = lbound(A[i]);
-        	   System.out.println("idx : " + idx);
-        	   longS[idx] = A[i];
-           }
-           
-           int ans = 0;
-           for (int i=0; i<N; i++) {
-        	   System.out.println(longS[i]);
-        	   ans = Math.max(longS[i], ans);
-           }
-           System.out.println(ans);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        // 입력 받기
+        N = Integer.parseInt(br.readLine());
+        A = new int[N];
+        dp = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        // dp 배열을 이용한 LIS (Longest Increasing Subsequence) 계산
+        int len = 0;
+        dp[len++] = A[0]; // 첫 번째 원소로 시작
+        for (int i = 1; i < N; i++) {
+            if (A[i] > dp[len - 1]) {
+                // 현재 수가 dp의 마지막 원소보다 크면, dp에 추가
+                dp[len++] = A[i];
+            } else {
+                // 그렇지 않으면, A[i] 이상의 값이 처음 등장하는 위치를 찾아 갱신
+                int idx = lowerBound(dp, 0, len - 1, A[i]);
+                dp[idx] = A[i];
+            }
+        }
+        
+        // dp의 길이가 곧 최장 증가 부분 수열의 길이
+        System.out.println(len);
     }
     
-    static int lbound(int num) {
-    	int left = 0;
-    	int right = longS.length;
-    	while (left < right) {
-    		int mid = (left + right) / 2;
-    		if (longS[mid] >= num) {
-    			right = mid;
-    		}
-    		else {
-    			left = mid+1;
-    		}
-    	}
-    	return N/2 - left ;
+    // lowerBound: arr[left...right] 구간에서 key 이상의 값이 처음 등장하는 인덱스 반환
+    static int lowerBound(int[] arr, int left, int right, int key) {
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (arr[mid] < key) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left;
     }
 }
-
-
-
