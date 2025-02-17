245688e48ab82ef4d036ead378edd900e1816bb2
palter09 <42296211+palter09@users.noreply.github.com>
2025-02-17 09:19:08 +0900

    Create 통근버스.cc

---
+++ HSAT/제 4회/통근버스.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int n;
+	cin >> n;
+
+	vector<int> arr(n);
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+
+	// more[i][k] -> i와 k사이에 A_i보다 큰게 몇게 있는지
+	vector<vector<int>> more(n, vector<int>(n));
+	int total = 0;
+	for (int i = 0; i < n; i++) {
+		for (int k = i + 1; k < n; k++) {
+			if (arr[i] < arr[k]) { // k가 아니라 j
+				more[i][k] = more[i][k - 1] + 1;
+			}
+
+			else {
+				more[i][k] = more[i][k - 1];
+				total += more[i][k];
+			}
+		}
+	}
+
+	cout << total;
+
+	return 0;
+}
