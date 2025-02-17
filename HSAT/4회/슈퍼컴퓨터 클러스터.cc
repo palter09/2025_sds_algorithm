3d2a3c6fd28b7c8df439fe19e11cf6a8687987b6
palter09 <42296211+palter09@users.noreply.github.com>
2025-02-17 10:44:43 +0900

    Update 슈퍼컴퓨터 클러스터.cc

--- HSAT/4회/슈퍼컴퓨터 클러스터.cc
+++ HSAT/4회/슈퍼컴퓨터 클러스터.cc
@@ -24,24 +24,25 @@ int main() {
 	ll low = 1, high = 2000000000LL;
 	while (low < high) {
 		int mid = (low + high + 1) / 2; // high = low+1 인 경우에 무한루프가 돌아서 +1 해줌
-		if (test(mid)) {
+		if (test(mid)) { // mid까지 성능을 올릴 수 있으면 low를 mid로 올림
 			low = mid;
 		}
-		else {
+		else { // mid가 안되면 high를 mid로 내림
 			high = mid - 1;
 		}
 	}
 
-	cout << low;
+	cout << low; // low와 high가 같아져서 출력되니 low 출력
 
 	return 0;
 }
 
+// 성능 x에 도달하기 위한 비용을 계산해서 B와 비교하고, 작거나 같은지 테스트하는 함수
 bool test(ll x) {
 	ll cost = 0;
 	for (int i = 0; i < N; i++) {
-		if (a[i] < x) {
-			cost += (x - a[i]) * (x - a[i]);
+		if (a[i] < x) { // 현재 성능이 원하는 성능보다 작은지 확인
+			cost += (x - a[i]) * (x - a[i]); // 작으면 성능을 올리고 비용 계산
 			if (cost > B) return false;
 		}
 	}
